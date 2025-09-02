0f68b6d81147c1bf521f447ddd8d772265550714
fabiotcm <fabiotarcisioccpa@gmail.com>
2025-09-01 22:30:57 -0300

    [ADD] Entidade ItemTotal

---
+++ src/main/java/com/squad13/apimonolito/models/ItemTotal.java
@@ -0,0 +1,30 @@
+package com.squad13.apimonolito.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_item_total")
+public class ItemTotal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_item_total")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_item", referencedColumnName = "id_item", nullable = false)
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_material", referencedColumnName = "id_material")
+    private Material material;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_marca", referencedColumnName = "id_marca")
+    private Marca marca;
+
+    @Column(name = "is_ativo")
+    private Boolean ativo;
+}

0185b60f1a3ce69cfb2ccff365c8708f37d59e1e
fabiotcm <fabiotarcisioccpa@gmail.com>
2025-09-01 23:30:04 -0300

    [ADD] Item e ItemAmbiente

--- src/main/java/com/squad13/apimonolito/models/ItemAmbiente.java
+++ src/main/java/com/squad13/apimonolito/models/ItemAmbiente.java
@@ -1,4 +1,26 @@
 package com.squad13.apimonolito.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_item_ambiente")
 public class ItemAmbiente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_item_ambiente")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_item", nullable = false)
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_ambiente", nullable = false)
+    private Ambiente ambiente;
+
+    @Column(name = "is_ativo")
+    private Boolean ativo;
 }

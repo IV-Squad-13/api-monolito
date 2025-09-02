09de0d4c5e5cbfa1ee8821f46cfa425697284e4a
fabiotcm <fabiotarcisioccpa@gmail.com>
2025-09-01 22:05:37 -0300

    [ADD] Entidade TipoItem

---
+++ src/main/java/com/squad13/apimonolito/models/TipoItem.java
@@ -0,0 +1,21 @@
+package com.squad13.apimonolito.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_tipo_item")
+public class TipoItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tipo_item")
+    private Long id;
+
+    @Column(name = "nm_tipo_item", length = 50)
+    private String nome;
+
+    @Column(name = "is_ativo")
+    private Boolean ativo;
+}

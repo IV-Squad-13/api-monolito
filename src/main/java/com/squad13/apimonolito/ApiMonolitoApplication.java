05f7a46b2622a7985884d1166d360770873070ce
rafael-men <rafaelgoncalves1822@gmail.com>
2025-09-03 14:37:03 -0300

    métodos e controllers

--- src/main/java/com/squad13/apimonolito/ApiMonolitoApplication.java
+++ src/main/java/com/squad13/apimonolito/ApiMonolitoApplication.java
@@ -2,8 +2,10 @@ package com.squad13.apimonolito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ApiMonolitoApplication {
 
 	public static void main(String[] args) {

22d29afcd973e4a05d3680bd510000499fb2ba0a
Fábio Tarcísio Cardoso Moura <1232153602@acd.grupotiradentes.com>
2025-09-04 20:31:40 -0300

    [ADD] Marca Controller

---
+++ src/main/java/com/squad13/apimonolito/controllers/MarcaController.java
@@ -0,0 +1,34 @@
+package com.squad13.apimonolito.controllers;
+
+import com.squad13.apimonolito.models.Marca;
+import com.squad13.apimonolito.services.MarcaService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/marca")
+public class MarcaController {
+
+    @Autowired
+    private MarcaService marcaService;
+
+    @GetMapping
+    public List<Marca> getAll(){
+        return marcaService.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Marca> getById(@PathVariable Long id){
+        return marcaService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}

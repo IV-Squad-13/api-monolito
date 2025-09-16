ae1fddb8923559761958de024d285842d283841b
Yuri-IQI <anthony.yuri.ff@gmail.com>
2025-09-15 22:04:15 -0300

    feat: migration v3

--- src/main/java/com/squad13/apimonolito/models/catalog/associative/ItemAmbiente.java
+++ src/main/java/com/squad13/apimonolito/models/catalog/associative/ItemAmbiente.java
@@ -2,7 +2,6 @@ package com.squad13.apimonolito.models.catalog.associative;
 
 import com.squad13.apimonolito.models.catalog.Ambiente;
 import com.squad13.apimonolito.models.catalog.Item;
-import com.squad13.apimonolito.util.AssociativeId;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -11,21 +10,17 @@ import lombok.Data;
 @Table(name = "tb_item_ambiente")
 public class ItemAmbiente {
 
-    @EmbeddedId
-    @AttributeOverrides({
-            @AttributeOverride(name = "relA", column = @Column(name = "id_item")),
-            @AttributeOverride(name = "relB", column = @Column(name = "id_ambiente"))
-    })
-    private AssociativeId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_item_ambiente")
+    private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("relA")
-    @JoinColumn(name = "id_item", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_item")
     private Item item;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("relB")
-    @JoinColumn(name = "id_ambiente", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_ambiente")
     private Ambiente ambiente;
 
     @Column(name = "is_obrigatorio")
--- src/main/java/com/squad13/apimonolito/models/editor/relational/associative/ComposicaoAmbiente.java
+++ src/main/java/com/squad13/apimonolito/models/editor/relational/associative/ComposicaoAmbiente.java
@@ -2,7 +2,6 @@ package com.squad13.apimonolito.models.editor.relational.associative;
 
 import com.squad13.apimonolito.models.catalog.associative.ItemAmbiente;
 import com.squad13.apimonolito.models.editor.relational.Empreendimento;
-import com.squad13.apimonolito.util.AssociativeId;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -11,18 +10,15 @@ import lombok.Data;
 @Table(name = "tb_composicao_ambiente")
 public class ComposicaoAmbiente {
 
-    @EmbeddedId
-    private AssociativeId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @MapsId("relA")
     @JoinColumn(name = "id_empreendimento")
     private Empreendimento empreendimento;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumns({
-            @JoinColumn(name = "id_item", referencedColumnName = "id_item"),
-            @JoinColumn(name = "id_ambiente", referencedColumnName = "id_ambiente")
-    })
+    @JoinColumn(name = "id_item_ambiente")
     private ItemAmbiente ambienteCompositor;
 }

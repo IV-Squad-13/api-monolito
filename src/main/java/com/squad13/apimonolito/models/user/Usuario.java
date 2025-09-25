46e82be3c5fae233ef3d14c978fb12a1f1882e79
fabiotcm <fabiotarcisioccpa@gmail.com>
2025-09-25 16:38:26 -0300

    [UPDATE] Usuário implements UserDetails

--- src/main/java/com/squad13/apimonolito/models/user/Usuario.java
+++ src/main/java/com/squad13/apimonolito/models/user/Usuario.java
@@ -2,11 +2,17 @@ package com.squad13.apimonolito.models.user;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "tb_usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +22,25 @@ public class Usuario {
     @Column(name = "nm_usuario", nullable = false, length = 50)
     private String nome;
 
+    @Column(name = "senha", nullable = false, length = 50)
+    private String senha;
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "fk_papel")
     private Papel papel;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.papel.getNome()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.nome;
+    }
 }

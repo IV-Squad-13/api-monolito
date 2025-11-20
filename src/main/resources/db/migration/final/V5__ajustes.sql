072f8552e763bca3552b6383c3255e8ce30ada68
rafael-men <rafaelgoncalves1822@gmail.com>
2025-11-20 11:38:39 -0300

    kubernetes done

--- src/main/resources/db/migration/final/V5__ajustes.sql
+++ src/main/resources/db/migration/final/V5__ajustes.sql
@@ -1,6 +1,11 @@
 ALTER TABLE tb_empreendimento
     ADD tp_init VARCHAR(20);
 
+-- Preencher valores NULL existentes com um valor padrão
+UPDATE tb_empreendimento
+SET tp_init = 'PADRAO'
+WHERE tp_init IS NULL;
+
 ALTER TABLE tb_empreendimento
     ALTER COLUMN tp_init SET NOT NULL;
 

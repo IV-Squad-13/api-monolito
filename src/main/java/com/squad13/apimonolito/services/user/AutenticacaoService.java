7780a8e851754fbbb685b0b0c00cfb29488664b0
Yuri-IQI <anthony.yuri.ff@gmail.com>
2025-12-02 15:07:07 -0300

    feat: email para criação de usuário

--- src/main/java/com/squad13/apimonolito/services/user/AutenticacaoService.java
+++ src/main/java/com/squad13/apimonolito/services/user/AutenticacaoService.java
@@ -1,12 +1,16 @@
 package com.squad13.apimonolito.services.user;
 
 import com.squad13.apimonolito.DTO.auth.register.RegisterDto;
+import com.squad13.apimonolito.DTO.auth.res.ResUserDTO;
 import com.squad13.apimonolito.exceptions.exceptions.InvalidTokenException;
+import com.squad13.apimonolito.exceptions.exceptions.ResourceAlreadyExistsException;
+import com.squad13.apimonolito.exceptions.exceptions.ResourceNotFoundException;
 import com.squad13.apimonolito.models.user.Papel;
 import com.squad13.apimonolito.models.user.Usuario;
 import com.squad13.apimonolito.repository.user.PapelRepository;
 import com.squad13.apimonolito.repository.user.UsuarioRepository;
 import com.squad13.apimonolito.util.enums.PapelEnum;
+import jakarta.mail.MessagingException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,22 +20,23 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AutenticacaoService implements UserDetailsService {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    private final UsuarioRepository usuarioRepository;
 
-    @Autowired
-    private PapelRepository papelRepository;
+    private final PapelRepository papelRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     private final TokenService tokenService;
+
+    private final EmailService emailService;
 
     public Usuario findMe(String authHeader) {
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
@@ -63,21 +68,18 @@ public class AutenticacaoService implements UserDetailsService {
     }
 
     @Transactional
-    public Usuario registerUser(RegisterDto dto) {
-
-        PapelEnum papelInformado = dto.getPapel();
-
+    public ResUserDTO registerUser(RegisterDto dto) throws IOException, MessagingException {
         if (usuarioRepository.existsByNome(dto.getNome())) {
-            throw new IllegalArgumentException("Nome de usuário já existe");
+            throw new ResourceAlreadyExistsException("Nome de usuário já existe");
         }
 
-        if (dto.getEmail() != null && !dto.getEmail().isEmpty() && usuarioRepository.existsByEmail(dto.getEmail())) {
-            throw new IllegalArgumentException("Email já cadastrado");
+        if (dto.getEmail() != null && !dto.getEmail().isBlank()
+                && usuarioRepository.existsByEmail(dto.getEmail())) {
+            throw new ResourceAlreadyExistsException("Email já cadastrado");
         }
 
-        // Valida o Papel
-        Papel papel = papelRepository.findByNome(papelInformado)
-                .orElseThrow(() -> new IllegalArgumentException("Papel não encontrado no sistema"));
+        Papel papel = papelRepository.findByNome(dto.getPapel())
+                .orElseThrow(() -> new ResourceNotFoundException("Papel não encontrado no sistema"));
 
         Usuario usuario = new Usuario();
         usuario.setNome(dto.getNome());
@@ -85,7 +87,26 @@ public class AutenticacaoService implements UserDetailsService {
         usuario.setEmail(dto.getEmail());
         usuario.setPapel(papel);
 
-        return usuarioRepository.save(usuario);
+        usuario = usuarioRepository.save(usuario);
+
+        if (usuario.getEmail() != null) {
+            String template = emailService.loadTemplate("created-user/created-user.html");
+            Map<String, String> vars = Map.of(
+                    "nome", usuario.getNome(),
+                    "username", usuario.getUsername(),
+                    "email", usuario.getEmail(),
+                    "senha", dto.getSenha()
+            );
+
+            String filledTemplate = emailService.applyVariables(template, vars);
+            emailService.sendHtmlMail(
+                    usuario.getEmail(),
+                    "Conta criada com sucesso",
+                    filledTemplate
+            );
+        }
+
+        return ResUserDTO.from(usuario);
     }
 }
 
--- src/main/java/com/squad13/apimonolito/services/user/EmailService.java
+++ src/main/java/com/squad13/apimonolito/services/user/EmailService.java
@@ -1,4 +1,55 @@
 package com.squad13.apimonolito.services.user;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendHtmlMail(String target, String subject, String html) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setTo(target);
+        helper.setSubject(subject);
+        helper.setText(html, true);
+        helper.setFrom("MS_XPacz6@test-vz9dlem59574kj50.mlsender.net");
+
+        mailSender.send(message);
+    }
+
+    public String loadTemplate(String fileName) {
+        try {
+            ClassPathResource resource =
+                    new ClassPathResource("templates/email/" + fileName);
+
+            return new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load template: " + fileName, e);
+        }
+    }
+
+    public String applyVariables(String template, Map<String, String> values) {
+        String result = template;
+        for (var entry : values.entrySet()) {
+            result = result.replace("{{" + entry.getKey() + "}}", entry.getValue());
+        }
+        return result;
+    }
 }
